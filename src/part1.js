ed9da239813264df14b211065dd624e1c1868eb1
venla <92327910+vvuskali@users.noreply.github.com>
2023-03-08 09:22:50 +0200

    Update part1.js

--- src/part1.js
+++ src/part1.js
@@ -1,5 +1,39 @@
 import React from 'react'
 
+const Header = (props) => {
+    return (
+      <div>
+        <h1>{course.name}</h1>
+      </div>
+    )
+  }
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.name} {props.exercises}</p>
+    </div>
+)
+}
+    
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={course.parts[0].name} exercises={course.parts[0].exercises}/>
+      <Part name={course.parts[1].name} exercises={course.parts[1].exercises}/>
+      <Part name={course.parts[2].name} exercises={course.parts[2].exercises}/>
+    </div>
+) 
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Total {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
+    </div>
+)
+}
+
 const App = () => {
   const course = { 
       name: 'Half Stack application development',
@@ -17,40 +51,6 @@ const App = () => {
         exercises: 14
       }
     ]
-  }
-
-  const Header = (props) => {
-    return (
-      <div>
-        <h1>{course.name}</h1>
-      </div>
-    )
-  }
-
-  const Part = (props) => {
-    return (
-      <div>
-        <p>{props.name} {props.exercises}</p>
-      </div>
-    )
-  }
-    
-  const Content = (props) => {
-    return (
-      <div>
-        <Part name={course.parts[0].name} exercises={course.parts[0].exercises}/>
-        <Part name={course.parts[1].name} exercises={course.parts[1].exercises}/>
-        <Part name={course.parts[2].name} exercises={course.parts[2].exercises}/>
-      </div>
-    )
-  }
-
-  const Total = (props) => {
-    return (
-      <div>
-        <p>Total {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
-      </div>
-    )
   }
 
   return (
